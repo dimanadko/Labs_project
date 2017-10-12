0aa10ff750775343c053ffe4b465765402692e08
dimanadko <dimanadobko@gmail.com>
2017-10-12 23:34:22 +0300

    task 1-4 done

--- main.js
+++ main.js
@@ -4,37 +4,38 @@ let clothes = [
   'jeans','t-shirt','blue jacket','trousers', 'pans',
   'shirt', 'red hoody', 'sweatshirt', 'black snapback', 'socks'
 ];
-console.log({clothes});
+console.log(clothes);
+console.log('----------')
 
 function DoubleWord(word) {
   if ((word.split(' ')).length === 2) {
-    console.log(word);
     return word;
   }
 };
-let complicated_clothes = clothes.map(DoubleWord);
+
+let complicated_clothes = clothes.filter(DoubleWord);
+
+console.log(complicated_clothes);
+console.log('----------')
+
 let sorted_clothes = clothes.sort();
+
 console.log(sorted_clothes);
-
-/*class InfWord {
-  constructor(frequancy, diftong){
-    this.frequancy = frequancy;
-    this.diftong = diftong;
-  };
-};*/
-
+console.log('----------')
 
 function InfWord (word){
   const frequancy = {};
   let char_arr = word.split('');
   let i;
   for (i of char_arr) {
-    if (frequancy[i]){
-      frequancy[i] +=1;
-    } else {
-      frequancy[i] = 1;
-    };
-  }
-  console.log(frequancy);
+    (frequancy[i])? frequancy[i] +=1 : frequancy[i] = 1;
+  };
+  let loud = word.split(/[^eyuioa]/g)
+  let diftounge = [];
+  for (i of loud){
+    if (i.length === 2){diftounge.push(i)};
+  };
+  return[frequancy, diftounge];
 };
-InfWord('sssplits');
+let inform_arr = clothes.map(InfWord);
+console.log(inform_arr);
